f883dcfc7ff39c83af6d51bb150119edac413059
ogunnaike folawe matthew <fivestonestech@gmail.com>
2023-04-16 16:13:12 +0100

    scroll reveal, animation using css keyframes
    after adding media query,i began to add scroll reveal using javascript and this makes the website appealling to the site,
    
    i also included animation CSS keyframes on the hero section which makes the image float.

--- js/pfolio.js
+++ js/pfolio.js
@@ -114,9 +114,18 @@ window.onscroll = () => {
 
 
 
+// scroll reveal
+ScrollReveal({
+    //  reset: true,
+     distance: '80px',
+     duration: 2000,
+     delay: 200
+});
 
-
-
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 
