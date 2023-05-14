8b785802a02112b64c18a8d9f203e1ab15e6a314
ogunnaike folawe matthew <fivestonestech@gmail.com>
2023-05-14 20:50:56 +0100

    //changes
    portfolio
    img

--- js/pfolio.js
+++ js/pfolio.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // This is for typed script js
 const typed = new Typed('.multiple-text',{
-   strings: ['Frontend Developer','YouTuber','Web Designer','Content Creator'],
+   strings: ['Frontend Developer','YouTuber','Web Designer','Content Creator','Entrepreneur'],
    typeSpeed: 100,
    backSpeed: 100,
    backDelay: 1000,
